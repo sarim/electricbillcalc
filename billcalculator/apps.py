661aeb4e48119e6fbfef67fd45e69b11a0acbd88
Sarim Khan <sarim2005@gmail.com>
2017-07-24 01:57:47 +0600

    remove python implementation

--- billcalculator/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BillcalculatorConfig(AppConfig):
-    name = 'billcalculator'
--- billcalculator/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class Myform(forms.Form):
-    Unit = forms.CharField(
-        label = "Enter your total unit",
-        max_length = 80,
-        required = True,
-    )
